28c63c208a7592714f781307175caa256f3ec376
Anna Ukolova <anikadd3@gmail.com>
2026-07-07 00:23:45 +0300

    new class OperatorHomework

---
+++ src/main/java/guru/qa/OperatorHomework.java
@@ -0,0 +1,32 @@
+package guru.qa;
+
+public class OperatorHomework {
+    public static void cleaning(String... args) {
+        // применить несколько арифметических операций ( + , -, * , /) над двумя примитивами типа int
+        int bottle = 3;
+        int garbage = 2;
+        System.out.println(bottle / garbage);
+        System.out.println(bottle * garbage);
+        System.out.println(bottle - garbage);
+        System.out.println(bottle + garbage);
+        // применить несколько арифметических операций над int и double в одном выражении
+        double paper = 3.8;
+        double take =  bottle + paper;
+        System.out.println(take/garbage);
+        System.out.println(++take*garbage);
+        System.out.println((take+garbage)+(bottle+paper)/2);
+        // применить несколько логических операций ( < , >, >=, <= )
+        System.out.println(take > paper);
+        System.out.println(bottle+garbage == take);
+        System.out.println(10 / 2.5 <= take);
+        //получить переполнение при арифметической операции
+        int pack = 128;
+        byte fullpackage = (byte)pack;
+        System.out.println(fullpackage);
+        System.out.println("что же я наделала?!");
+        }
+
+    public static void main(String[] args) {
+        cleaning();
+    }
+}
